d1b793c39a5ed4fc775c4375f85d3f484a7b3c3a
Jesus A. G <xtornasol512@gmail.com>
2017-04-11 02:37:45 -0500

    fix allowed host

--- garzonfilms/garzonfilms/settings.py
+++ garzonfilms/garzonfilms/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ast.literal_eval(os.environ['DEBUG_STATE'])
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 PRODUCTION = ast.literal_eval(os.environ['PRODUCTION'])
 
