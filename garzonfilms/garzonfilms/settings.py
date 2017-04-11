3fa688f06713a59f5a2d5ad84cb5a518fd16de39
Jesus A. G <xtornasol512@gmail.com>
2017-04-11 02:31:00 -0500

    add whitenoise app for staticfiles

--- garzonfilms/garzonfilms/settings.py
+++ garzonfilms/garzonfilms/settings.py
@@ -111,6 +111,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
