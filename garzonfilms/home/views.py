bd3da9d65b3893876c265d59d534a7cd252d9593
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-08-01 00:49:11 -0500

    add templates and views changes to work with template extends

--- garzonfilms/home/views.py
+++ garzonfilms/home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def hola(request):
     return render(request, "index.html", {})
+
+def contact(request):
+    return render(request, "contact.html", {})
+
+def work(request):
+    return render(request, "work.html", {})
+
+def about(request):
+    return render(request, "about.html", {})
