e2cb1610c3cc9f2eda210214332b6c23894a88b8
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-08-01 01:52:11 -0500

    fix databseurl

--- garzonfilms/garzonfilms/settings.py
+++ garzonfilms/garzonfilms/settings.py
@@ -71,8 +71,7 @@ WSGI_APPLICATION = 'garzonfilms.wsgi.application'
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES = {}
-DATABASES['default'].update(db_from_env)
+DATABASES={'default': db_from_env }
 
 
 # Password validation
